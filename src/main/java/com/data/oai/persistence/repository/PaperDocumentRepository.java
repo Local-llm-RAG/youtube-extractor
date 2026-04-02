979e4b6f60aaa925c23d71be41b2abf2d6c7cc04
Martin <martinnevexis99@gmail.com>
2026-04-02 20:54:29 +0300

    Log stored content size + Fix some startup issues

--- src/main/java/com/data/oai/persistence/repository/PaperDocumentRepository.java
+++ src/main/java/com/data/oai/persistence/repository/PaperDocumentRepository.java
@@ -2,6 +2,19 @@ package com.data.oai.persistence.repository;
 
 import com.data.oai.persistence.entity.PaperDocumentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PaperDocumentRepository extends JpaRepository<PaperDocumentEntity, Long> {
+
+    @Query(value = """
+            SELECT COALESCE(SUM(
+                OCTET_LENGTH(COALESCE(tei_xml, '')) +
+                OCTET_LENGTH(COALESCE(raw_content, '')) +
+                OCTET_LENGTH(COALESCE(title, '')) +
+                OCTET_LENGTH(COALESCE(abstract, ''))
+            ), 0)
+            + (SELECT COALESCE(SUM(OCTET_LENGTH(COALESCE(text, ''))), 0) FROM document_section)
+            FROM record_document
+            """, nativeQuery = true)
+    long sumStoredContentBytes();
 }
